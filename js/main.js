e78c43e441ffe3e5b009ac941759203567d4f804
Alex <alexherbert.123@outlook.com>
2022-08-27 00:13:24 +0100

    This should work

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
+const encodedParams = new URLSearchParams();
+encodedParams.append("id", "1");
+
 const options = {
 	method: 'POST',
 	headers: {
-		'X-RapidAPI-Key': '395c6962-00be-4ba1-b7a3-f37afd2497c9',
+		'content-type': 'application/x-www-form-urlencoded',
+		'X-RapidAPI-Key': '755eec32bbmsh511028bc83d44afp17d90djsn0fd2750c913f',
 		'X-RapidAPI-Host': 'CoinMarketCapzakutynskyV1.p.rapidapi.com'
-	}
+	},
+	body: encodedParams
 };
 
 fetch('https://coinmarketcapzakutynskyv1.p.rapidapi.com/getCryptocurrency', options)
