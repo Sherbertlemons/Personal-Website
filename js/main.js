1cfa3be803750eda9a05cf0cdfff9779a1fec986
Alex <alexherbert.123@outlook.com>
2022-08-27 23:49:32 +0100

    Adding etherum API page

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-const encodedParams = new URLSearchParams();
-encodedParams.append("id", "1");
 
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'X-RapidAPI-Key': '755eec32bbmsh511028bc83d44afp17d90djsn0fd2750c913f',
-		'X-RapidAPI-Host': 'CoinMarketCapzakutynskyV1.p.rapidapi.com'
-	},
-	body: encodedParams
-};
+var eth = document.getElementById("ethereum");
 
-fetch('https://coinmarketcapzakutynskyv1.p.rapidapi.com/getCryptocurrency', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+var liveprice = {
+	"async": true,
+	"scroosDomain": true,
+	"url": "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd",
+
+	"method": "GET",
+	"headers": {}
+}
+
+$.ajax(liveprice).done(function (response){
+	eth.innerHTML = response.ethereum.usd;
+});
