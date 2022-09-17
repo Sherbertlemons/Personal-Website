368ae36795946a1708e23cf93d230dd73b848f21
Alex <alexherbert.123@outlook.com>
2022-09-17 23:18:21 +0100

    Updating crypto page to now include bitcoin

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 
 var eth = document.getElementById("ethereum");
+var btc = document.getElementById("bitcoin");
 
 var liveprice = {
 	"async": true,
 	"scroosDomain": true,
-	"url": "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd",
-
+	"url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Clitecoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+	
 	"method": "GET",
 	"headers": {}
 }
 
 $.ajax(liveprice).done(function (response){
 	eth.innerHTML = response.ethereum.usd;
+	btc.innerHTML = response.bitcoin.usd;
 });
